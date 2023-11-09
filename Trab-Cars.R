5db7f033f2bda891ec61b697a41099d3ff71b6d5
Dr-Caponero <caponero@gmail.com>
2023-11-09 16:42:06 -0300

    corrigindo o dataset

--- Trab-Cars.R
+++ Trab-Cars.R
@@ -1,8 +1,6 @@
-library(ggplot2)
-library(dplyr)
 library(tidyverse)
 
-dados = CarPrice_Assignment
+dados <- read_csv("CarPrice_Assignment.csv")
 
 head(dados)
 
@@ -42,3 +40,44 @@ dados %>% pivot_longer(cols = colnames(dados)[c(4,5,6,7,8,9,15,16,18)],
   #geom_text(position = position_stack(vjust = .5), color = "#FFFFFF")+
   theme_light()+
   theme(strip.text.x = element_text(colour = "black"))
+
+
+# separando a marca do nome do carro
+aux <- str_split(dados$CarName, " ", n = 2)
+aux1 <- NULL
+aux2 <- NULL
+for(i in 1:205){
+  aux1[i] <- aux[[i]][1]
+  aux2[i] <- aux[[i]][2]
+}
+
+dados$marca <- aux1
+dados$nome <- aux2
+
+# Corrigindo corrigindo a marca de alguns carros
+
+# maxda = mazda
+aux <- dados |> dplyr::filter(marca=="maxda") |> mutate(marca="mazda")
+dados <- dados |> dplyr::filter(marca!="maxda")
+dados <- rbind(dados,aux)
+# Nissan = nissan
+aux <- dados |>  dplyr::filter(marca=="Nissan") |> mutate(marca="nissan")
+dados <- dados |> dplyr::filter(marca!="Nissan")
+dados <- rbind(dados,aux)
+# porsche = porcshce
+dados |> dplyr::filter(marca=="porcshce") |> mutate(marca="porsche")
+dados <- dados |> dplyr::filter(marca!="porcshce")
+dados <- rbind(dados,aux)
+# toyota = toyouta
+aux <- dados |> dplyr::filter(marca=="toyouta") |> mutate(marca="toyota")
+dados <- dados |> dplyr::filter(marca!="toyouta")
+dados <- rbind(dados,aux)
+# vokswagen = volkswagen = vw
+aux <- dados |> dplyr::filter(marca=="vokswagen") |> mutate(marca="vw")
+dados <- dados |> dplyr::filter(marca!="vokswagen")
+dados <- rbind(dados,aux)
+aux <- dados |> dplyr::filter(marca=="volkswagen") |> mutate(marca="vw")
+dados <- dados |> dplyr::filter(marca!="volkswagen")
+dados <- rbind(dados,aux)
+
+
